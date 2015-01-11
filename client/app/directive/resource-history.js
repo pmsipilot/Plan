5a164385a1da7b560de900ebd5833d00716afa59
jubianchi <julien.bianchi@pmsipilot.com>
2015-01-11 20:16:59 +0100

    Automatically load project for project_delivery history entries

--- client/app/directive/resource-history.js
+++ client/app/directive/resource-history.js
@@ -8,50 +8,60 @@ angular.module('pmsiplan').directive('resourceHistory', ['AngularDataStore', fun
         link: function(scope, element, attrs) {
             var projects = {},
                 deliveries = {},
+                getDelivery = function(entry) {
+                    if(deliveries[entry.content.delivery]) {
+                        entry.delivery = deliveries[entry.content.delivery];
+                    } else {
+                        AngularDataStore.findBy('delivery', {_id: entry.content.delivery}).then(function(delivs) {
+                            deliveries[entry.content.delivery] = delivs[0];
+
+                            entry.delivery = deliveries[entry.content.delivery];
+                        });
+                    }
+                },
+                getProject = function(entry) {
+                    if(projects[entry.content.project]) {
+                        entry.project = projects[entry.content.project];
+                    } else {
+                        AngularDataStore.findBy('project', {_id: entry.content.project}).then(function(proj) {
+                            projects[entry.content.project] = proj[0];
+
+                            entry.project = projects[entry.content.project];
+                        });
+                    }
+                },
                 callback = function(history) {
-                scope.history = history
-                    .map(function(entry) {
-                        entry.content = JSON.parse(entry.content);
+                    scope.history = history
+                        .map(function(entry) {
+                            entry.content = JSON.parse(entry.content);
 
-                        return entry;
-                    })
-                    .filter(function(entry) {
-                        return !scope.resourceId || entry.content._id === scope.resourceId;
-                    })
-                    .map(function(entry) {
-                        entry.showContent = function() {
-                            if (!entry.isContentVisible) {
-                                if(entry.content.project) {
-                                    if(projects[entry.content.project]) {
-                                        entry.project = projects[entry.content.project];
-                                    } else {
-                                        AngularDataStore.findBy('project', {_id: entry.content.project}).then(function(proj) {
-                                            projects[entry.content.project] = proj[0];
+                            return entry;
+                        })
+                        .filter(function(entry) {
+                            return !scope.resourceId || entry.content._id === scope.resourceId;
+                        })
+                        .map(function(entry) {
+                            entry.showContent = function() {
+                                if (!entry.isContentVisible) {
+                                    if(entry.content.project) {
+                                        getProject(entry);
+                                    }
 
-                                            entry.project = projects[entry.content.project];
-                                        });
+                                    if(entry.content.delivery) {
+                                        getDelivery(entry);
                                     }
                                 }
 
-                                if(entry.content.delivery) {
-                                    if(deliveries[entry.content.delivery]) {
-                                        entry.delivery = deliveries[entry.content.delivery];
-                                    } else {
-                                        AngularDataStore.findBy('delivery', {_id: entry.content.delivery}).then(function(delivs) {
-                                            deliveries[entry.content.delivery] = delivs[0];
+                                entry.isContentVisible = !entry.isContentVisible;
+                            };
 
-                                            entry.delivery = deliveries[entry.content.delivery];
-                                        });
-                                    }
-                                }
+                            if(entry.resource == 'project_delivery') {
+                                getProject(entry);
                             }
 
-                            entry.isContentVisible = !entry.isContentVisible;
-                        };
-
-                        return entry;
-                    });
-            };
+                            return entry;
+                        });
+                };
 
             if(attrs.type) {
                 scope.type = attrs.type;
